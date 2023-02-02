8af7bd7ea7fc01cd5521307b84d1cb23f0051484
Anna Gan <anna_gan@epam.com>
2023-02-02 15:50:54 +0800

    feat: add configure file path basic resolve Api

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -2,8 +2,11 @@ import { createServer as createViteServer } from 'vite';
 import { pluginIndexHtml } from './plugin-island/indexHtml';
 import pluginReact from '@vitejs/plugin-react';
 import { PACKAGE_ROOT } from './constants';
+import { resolveConfig } from './config';
 
 export async function createDevServer(root = process.cwd()) {
+  const config = await resolveConfig(root, 'serve', 'development');
+  console.log(config);
   return createViteServer({
     root,
     plugins: [pluginIndexHtml(), pluginReact()],
