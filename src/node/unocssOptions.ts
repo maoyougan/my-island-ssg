abe0821b645366f6c83a44644a05b1078c7b0e39
Anna Gan <anna_gan@epam.com>
2023-02-09 17:47:23 +0800

    feat: add unocss

---
+++ src/node/unocssOptions.ts
@@ -0,0 +1,8 @@
+import type { VitePluginConfig } from 'unocss/vite';
+import { presetAttributify, presetWind, presetIcons } from 'unocss';
+
+const options: VitePluginConfig = {
+  presets: [presetAttributify(), presetWind({}), presetIcons()]
+};
+
+export default options;
--- src/theme-default/Layout/index.tsx
+++ src/theme-default/Layout/index.tsx
@@ -1,8 +1,11 @@
 import { Content } from '@runtime';
+import 'virtual:uno.css'; // unocss自身问题，官方建议虚拟模块导入
 export function Layout() {
   return (
     <div>
-      <h1>Common Content</h1>
+      <h1 p="2" m="4">
+        Common Content
+      </h1>
       <Content />
     </div>
   );
